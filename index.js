6009710437c82d0e0f5bea469733e410a62c7d7c
ckhajavi <cara.khajavi@gmail.com>
2016-06-11 01:32:37 -0700

    fizing hash key

--- index.js
+++ index.js
@@ -11,14 +11,37 @@ var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
 var request = require('superagent');
-var meetAPP_ID = 1016247148423251;
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var Crypto = require("crypto-js");
-//var middleWare = require('./middleWare.js');
 var unirest = require('unirest');
+
+var jwt = require('jsonwebtoken');
+var config = require('./config'); //gt out config file
+var User = require('./models/user'); //get our mongoose model
+
+mongoose.connect(config.database); //connect to db
+app.set('superSecret', config.secret); //secret variable 
+var meetAPP_ID = 1016247148423251;
+
+//use body parser so that we can get info from POST and/or URL params
+app.use(bodyParser.urlencoded({extended: false}));
+
 var APP_SECRET = '901271404bee861b0810763d5d4ca8d4';
 
 
 app.set('port', (process.env.PORT || 5000));
+
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+// API ROUTES -------------------
+
+// get an instance of the router for api routes
+var apiRoutes = express.Router(); 
+
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+
+// TODO: route middleware to verify a token
+
 
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -36,6 +59,104 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+//test the database
+app.get('/api/setup', function(req, res) {
+
+  // create a sample user
+  var nick = new User({ 
+    name: 'Cara Khajavi', 
+    password: 'password',
+    admin: true 
+  });
+
+  // save the sample user
+  nick.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
+// route middleware to verify a token
+apiRoutes.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+    
+  }
+});
+
+apiRoutes.post('/authenticate', function(req, res) {
+
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign(user, app.get('superSecret'), {
+          //expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+
+    }
+
+  });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});   
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
 
 //returns the number of mutual friends
 app.get('/api/amf/:access_token/:friend_id',function(request,response){
@@ -132,16 +253,11 @@ app.get('fb/event/:edgeid/:access_token',function(request,response){
 	response.send('event search...coming soon');
 });
 
-
-
 //TO:DO
 //app route info
 app.get('/info',function(request,response){
 	response.send('info about routes...coming soon');
 });
-
-
-
 
 
 //test routes begin
